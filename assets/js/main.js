979242f91979d4daf376f388ddfb17dd6f9041c0
MD <dhmedy@googlemail.com>
2022-08-12 13:57:42 +0200

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,17 +12,27 @@ let getraenke = [
 let newGetraenke =
     getraenke.map(
         function myFn(val, key, arr) {
-            console.log('val', val.toUpperCase());
-            console.log('key', key);
-            console.log('arr', arr);
+            console.log('val: ', val.toUpperCase());
+            console.log('key: ', key);
+            console.log('arr: ', arr);
         });
-console.log(newGetraenke);
+console.log('newGetraenke: ', newGetraenke);
 
 let ArrNewGetraenke = getraenke.map(
     ArrMyFn = (val, key, arr) => {
-        console.log('val', val.toUpperCase());
-        console.log('key', key);
-        console.log('arr', arr);
+        console.log('val: ', val.toUpperCase());
+        console.log('key: ', key);
+        console.log('arr: ', arr);
     });
-console.log(ArrNewGetraenke);
+console.log('ArrNewGetraenke: ', ArrNewGetraenke);
+
+
+console.log("----------------------------------------");
+
+let upperDrinks = getraenke.map((item) => {
+    console.log(item);
+    return item.toUpperCase()
+});
+
+console.log('upperDrinks: ', upperDrinks);
 
